b9fd7844b30d01cb46cc5defb580bd55f60f6d75
MariaBonde <mariabonde99@gmail.com>
2024-05-19 19:29:23 +0200

    implemented new methods, also made unit tests

--- src/FlushablePromise.js
+++ src/FlushablePromise.js
@@ -1,77 +1,169 @@
 // @flow
 
-export default class FlushablePromise<T,U=T> {
-    _promise: Promise<T>;
-    resolve: (T) => void;
-    reject: (any) => void;
-    _flush: ?[(() => U),FlushablePromise<any>]; //tuple of the flushing function and the promise to resolve it with
-    isResolvedOrRejected: boolean;
+export default class FlushablePromise<T> {
+  _promise: Promise<T>;
+  resolve: (T) => void;
+  reject: (any) => void;
+  _fetchValue: (() => T); //tuple of the flushing function and the promise to resolve it with
 
-   
-    /**
-     * onFlush parameter: only used for first promise of chain if one wants flushable option
-     * fromFlushable parameter: only used for chained promises, tuple of flush-function and the first promise in the chain
-     */
-    constructor(onFlush?: (() => U), fromFlushable?: [(()=>U),FlushablePromise<U>]) {
-        this._promise = new Promise((resolve, reject) => {
-            this.resolve = (v) => {
-              resolve(v);
-              this.isResolvedOrRejected = true; //needed a way to check if a promise is settled
-            };
-            this.reject = (e) => {
-              reject(e);
-              this.isResolvedOrRejected = true;
-            };
-          });
+ 
+  /**
+   * onFlush parameter: only used for first promise of chain if one wants flushable option
+   * fromFlushable parameter: only used for chained promises, tuple of flush-function and the first promise in the chain
+   */
+  constructor(onFlush?: (() => T)) {
+    let { promise, resolve, reject } = Promise.withResolvers();
+    [this._promise, this.resolve, this.reject] = [promise,resolve,reject];
+    
+      if (onFlush) {this._fetchValue = onFlush} 
+
+  }
+  
+  then<ResolveType=T,RejectType=empty>(onFulfilled: (T) => (ResolveType), onRejected?: (any) => (RejectType)): FlushablePromise<ResolveType | RejectType> {
+      let newPromise = new FlushablePromise<ResolveType | RejectType>();
+      newPromise._setSourcePromise(this)
+
+      this._promise.then( //calling the old (this promise) promises then 
+      x => {//side 88
+        let fulfilledVal = onFulfilled(x);
+        if (fulfilledVal instanceof FlushablePromise) {
+          fulfilledVal._setSourcePromise(this)
+          newPromise._setSourcePromise(fulfilledVal)
+          fulfilledVal.resolve(x)
+          return newPromise.resolve(fulfilledVal)
+        }
+        else {
+          return newPromise.resolve(fulfilledVal)
+        }
+      }, //resolve the new promise with the result of the onfulfilled function
+        y => {
+          if (onRejected) {
+            let rejectedVal = onRejected(y)
+            if (rejectedVal instanceof FlushablePromise) {
+              rejectedVal._setSourcePromise(this)
+              newPromise._setSourcePromise(rejectedVal)
+              rejectedVal.reject(y)//????
+              return newPromise.resolve(rejectedVal)
+            }
+            else {return newPromise.resolve(rejectedVal)}
+          }
+          else {return newPromise.reject(y)}
+        }   //resolve the new promise 
+    ); 
       
-        if (fromFlushable) {this._flush = fromFlushable} //will only happen if constructor is used from inside then
-        else if (onFlush) {this._flush = [onFlush, this]}  //the first promise of the chain is created
-        this.isResolvedOrRejected = false;
-
+      return newPromise
     }
 
+  /**
+   * 
+   * Returns a new Promise. 
+   * This new promise is always pending when returned, 
+   * regardless of the current promise's status. 
+   * It's eventually rejected if onRejected throws an error (this is the reason for the ) or returns
+   * a Promise which is itself rejected; otherwise, it's eventually fulfilled.
+   */
+  catch<RejectType>(onRejected: ((x: any) => RejectType)): FlushablePromise<T | RejectType> {
+    return this.then(x=>x, onRejected)//returner bare seg selv om catch ikke
 
+  }
 
-
-
-    
-    then<ResolveType=T,RejectType=empty>(onFulfilled: (T) => (ResolveType), onRejected?: (any) => (RejectType)): FlushablePromise<ResolveType | RejectType, U> {
-        let newPromise = this._flush ? new FlushablePromise<ResolveType | RejectType,U>(undefined,[this._flush[0], this._flush[1]]) :  new FlushablePromise<ResolveType | RejectType,U>()
-
-        this._promise.then( //calling the old (this promise) promises then 
-            x => newPromise.resolve(onFulfilled(x)), //resolve the new promise with the result of the onfulfilled function
-            y => onRejected ? newPromise.resolve(onRejected(y)) : newPromise.reject(y) //resolve the new promise 
-        ); 
-        
-        return newPromise
-      }
-
-    /**
-     * 
-     * Returns a new Promise. 
-     * This new promise is always pending when returned, 
-     * regardless of the current promise's status. 
-     * It's eventually rejected if onRejected throws an error (this is the reason for the ) or returns
-     * a Promise which is itself rejected; otherwise, it's eventually fulfilled.
-     */
-    catch<RejectType>(onRejected: ((x: any) => RejectType)): FlushablePromise<T | RejectType,U> {
-      return this.then(x=>x, onRejected)//returner bare seg selv om catch ikke
-
+  finally(onFinally: ()=>void | any): FlushablePromise<any>{
+    if (typeof onFinally != 'function') {
+      return this.then(onFinally,onFinally)
     }
+    else {
+      return this.then(
+        (value) => FlushablePromise.resolve(onFinally()).then(()=>value),
+        (reason) =>
+          FlushablePromise.resolve(onFinally()).then(()=> {
+            throw reason;
+          })
+      )
+    }
+  }
 
-    /**
-     * check if _flush exists, then resolves original promise with the flush function
-     */
-    flush():void {
-      if (this._flush && this._flush.length ==2) {
-        let retrieveValueFunction = this._flush[0];
-        let originalPromise = this._flush[1];
-        originalPromise.resolve(retrieveValueFunction())
+  /**
+   * check if _flush exists, then resolves original promise with the flush function
+   */
+  flush():void {
+    if (this._fetchValue) {
+      this.resolve(this._fetchValue())
     }
+    else if (this._sourcePromise) {
+      this._sourcePromise.forEach((flushable)=>flushable.flush());
     }
-    
+  }
+
+  _setSourcePromise(sourcePromise:FlushablePromise<any>):void {
+    this._sourcePromise = [sourcePromise]
 }
 
+ _setSourcePromises(sourcePromise:FlushablePromise<any>[]):void {
+  if (!this._sourcePromise) {
+    this._sourcePromise = sourcePromise
+  }
+  else {
+    this._sourcePromise = this._sourcePromise.concat(sourcePromise);
+  }
+}
+
+static all(promiseList:FlushablePromise<any>[]): FlushablePromise<any[]> {
+  let counter:number = promiseList.length;
+  let arr = [];
+  let promise = new FlushablePromise<any[]>;
+  promise._setSourcePromises(promiseList) //every promise or just the flushable/the ones that have source promises?
+  for (let i = 0; i < counter; i++) {
+    promiseList[i].then((v) => {
+      arr[i] = v;
+      if(--counter == 0) {
+        promise.resolve(arr)
+      }
+    },(a)=>promise.reject(a))
+  }
+  return promise
+}
+
+
+static race(promiseList: FlushablePromise<any>[]): FlushablePromise<any> {
+  let promise = new FlushablePromise<any>
+  promise._setSourcePromises(promiseList)
+  promiseList.forEach((p)=>p.then((v)=>promise.resolve(v),(e)=>promise.reject(e)))
+  return promise
+}
+
+static any(promiseList: FlushablePromise<any>[]): FlushablePromise<any> {
+  let promise = new FlushablePromise<any>
+  let rejects = []
+  promise._setSourcePromises(promiseList)
+  promiseList.forEach((p) => p.then(
+      (v)=>promise.resolve(v),
+      (e)=>{
+        rejects.push(e)
+        if (rejects.length == promiseList.length) {
+          promise.reject(new AggregateError(rejects))
+        }
+      }))
+  
+  return promise
+}
+
+static resolve(value:any): FlushablePromise<any> {
+  if (value instanceof FlushablePromise) {
+    return value
+  }
+  let promise = new FlushablePromise<any>;
+  promise.resolve(value);
+  return promise
+
+}
+
+static reject(value:any): FlushablePromise<any> {
+  let promise = new FlushablePromise<any>;
+  promise.reject(value)
+  return promise
+}
+  
+}
 
 let f_promise_1 = new FlushablePromise<number>(()=>32);//flushing function is then ()=>32
 let f_promise_2 = f_promise_1.then((x => x*2));
